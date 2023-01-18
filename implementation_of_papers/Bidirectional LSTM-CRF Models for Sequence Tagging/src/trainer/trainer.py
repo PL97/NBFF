23669f31f56ac96e6a4d4832d78c924b85151952
PL97 <pengle@126.com>
2023-01-18 09:27:39 -0800

    clean up trainer

--- implementation_of_papers/Bidirectional LSTM-CRF Models for Sequence Tagging/src/trainer/trainer.py
+++ implementation_of_papers/Bidirectional LSTM-CRF Models for Sequence Tagging/src/trainer/trainer.py
@@ -6,6 +6,7 @@ from sklearn.metrics import precision_score, recall_score
 import numpy as np
 import wandb
 from seqeval.metrics import classification_report
+from tqdm import tqdm
 
 from utils.parse_summary import parse_summary
 
@@ -26,96 +27,96 @@ class trainer_base(pl.LightningModule):
                 {'params': self.model.parameters(), 'lr': self.args.learning_rate}
                 ])
         return optimizer
-    
-    def training_step(self, batch, batch_idx):
-        X, y = batch
-        y = y.long()
-        loss = self.model.loss(X, y)
-        score, pred = self.model(X)
-        self.log("train/loss", loss)
-        return {'loss': loss, 'preds': pred, 'target': y}
-    
+ 
+     
     def backward(self, loss, optimizer, optimizer_idx):
         loss.backward()
         torch.nn.utils.clip_grad_norm_(self.model.parameters(), 1.0) ## optional
         
     
-    @torch.no_grad()
-    def test_step(self, batch, batch_idx):
-        # this is the test loop
+    def _shared_step(self, batch, batch_idx, prefix, log=True):
         X, y = batch
         y = y.long()
         loss = self.model.loss(X, y)
         score, pred = self.model(X)
-        self.log("test/loss", loss)
+        if log:
+            self.log(f"{prefix}/loss", loss)
         return {'loss': loss, 'preds': pred, 'target': y}
+    
+    def training_step(self, batch, batch_idx):
+        return self._shared_step(batch, batch_idx, prefix="train")
+
+    
+    @torch.no_grad()
+    def test_step(self, batch, batch_idx):
+        # this is the test loop
+        return self._shared_step(batch, batch_idx, prefix="test")
         
     
     @torch.no_grad()
     def validation_step(self, batch, batch_idx):
         # this is the validation loop
-        X, y = batch
-        y = y.long()
-        loss = self.model.loss(X, y)
-        score, pred = self.model(X)
-        self.log("val/loss", loss, sync_dist=True)
-        return {'loss': loss, 'preds': pred, 'target': y}
+        return self._shared_step(batch, batch_idx, prefix="val")
     
+    def _eval(self, outputs, prefix="train", log=True):
+        preds, target, pred_orig, target_orig = [], [], [], []
+        for out in outputs:
+            preds.extend(out['preds'])
+            target.extend(out['target'].detach().cpu().tolist())
+        preds = [item for sublist in preds for item in sublist]
+        target = [item for sublist in target for item in sublist]
+        # update and log
+        for p, t in zip(preds, target):
+            if self.args.ids_to_labels[t] not in ['<START>', '<STOP>', '<PAD>']:
+                pred_orig.append(self.args.ids_to_labels[p])
+                target_orig.append(self.args.ids_to_labels[t])
+        report = classification_report([pred_orig], [target_orig], zero_division=0)
+        report_dict = parse_summary(report)
+        if log:
+            self.log(f'{prefix}/macro_avg/precision', report_dict['macro avg']['precision'])
+            self.log(f'{prefix}/macro_avg/recall', report_dict['macro avg']['recall'], sync_dist=True)
+            self.log(f'{prefix}/macro_avg/f1-score', report_dict['macro avg']['f1-score'], sync_dist=True)
+        return report_dict
         
     @torch.no_grad()
     def training_epoch_end(self, outputs):
-        preds, pred_orig, target, target_orig = [], [], [], []
-        for out in outputs:
-            preds.extend(out['preds'])
-            target.extend(out['target'].detach().cpu().tolist())
+        self._eval(outputs, prefix="train")
         
-        preds = [item for sublist in preds for item in sublist]
-        target = [item for sublist in target for item in sublist]
-        # update and log
-        for p, t in zip(preds, target):
-            if self.args.ids_to_labels[t] not in ['<START>', '<STOP>', '<PAD>']:
-                pred_orig.append(self.args.ids_to_labels[p])
-                target_orig.append(self.args.ids_to_labels[t])
-        report = classification_report([pred_orig], [target_orig], zero_division=0)
-        report_dict = parse_summary(report)
-        self.log('train/macro_avg/precision', report_dict['macro avg']['precision'])
-        self.log('train/macro_avg/recall', report_dict['macro avg']['recall'], sync_dist=True)
-        self.log('train/macro_avg/f1-score', report_dict['macro avg']['f1-score'], sync_dist=True)
     
     @torch.no_grad()
     def validation_epoch_end(self, outputs):
-        preds, pred_orig, target, target_orig = [], [], [], []
-        for out in outputs:
-            preds.extend(out['preds'])
-            target.extend(out['target'].detach().cpu().tolist())
+        self._eval(outputs, prefix="val")
         
-        preds = [item for sublist in preds for item in sublist]
-        target = [item for sublist in target for item in sublist]
-        # update and log
-        for p, t in zip(preds, target):
-            if self.args.ids_to_labels[t] not in ['<START>', '<STOP>', '<PAD>']:
-                pred_orig.append(self.args.ids_to_labels[p])
-                target_orig.append(self.args.ids_to_labels[t])
-        report = classification_report([pred_orig], [target_orig], zero_division=0)
-        report_dict = parse_summary(report)
-        self.log('val/macro_avg/precision', report_dict['macro avg']['precision'], sync_dist=True)
-        self.log('val/macro_avg/recall', report_dict['macro avg']['recall'], sync_dist=True)
-        self.log('val/macro_avg/f1-score', report_dict['macro avg']['f1-score'], sync_dist=True)
     
+    @torch.no_grad()
+    def test_epoch_end(self, outputs):
+        aggregation = "macro avg"
+        report_dict = self._eval(outputs, log=False)
+        wandb.run.summary.update({
+            f"test/{aggregation}/precision": report_dict['macro avg']['precision'], \
+            f"test/{aggregation}/recall": report_dict['macro avg']['recall'], \
+            f"test/{aggregation}/f1-score": report_dict['macro avg']['f1-score'] 
+        }) 
+        return report_dict
+    
+        
+        
     # @torch.no_grad()
-    # def test(self, dataloader):
+    # def test(self, dataloader, prefix="test", aggregation="macro avg"):
     #     self.model.eval()
-    #     m = nn.Softmax(dim=1)
-    #     prediction = []
-    #     labels = []
-    #     for X, y in dataloader:
+    #     self.model = self.model.to(self.device)
+    #     preds, target = ['None'] * 1
+    #     for X, y in tqdm(dataloader):
     #         X, y = X.to(self.device), y.to(self.device)
-    #         prediction.extend((m(self.model(X))[:, 1].detach().cpu().numpy() >= 0.5).astype(int))
-    #         labels.extend(y)
-            
-    #     prediction = np.stack(prediction, axis=0).reshape(-1, 1)
-    #     labels = torch.stack(labels, axis=0).detach().cpu().numpy()
-    #     TP = int(prediction.T@(labels==1).astype(int))
-    #     precision = 1.0*TP/np.sum(prediction)
-    #     recall = 1.0*TP/np.sum(labels==1)
-    #     return precision, recall
+    #         preds.add(self.model(X))
+    #         target.add(y)
+    #     print(preds)
+    #     target = [t.detach().cpu().tolist() for t in target]
+    #     preds = [item for sublist in preds for item in sublist]
+    #     target = [item for sublist in target for item in sublist]
+    #     report_dict = self._eval(preds=preds, target=target)
+    #     wandb.run.summary.update({
+    #         f"{prefix}/{aggregation}/precision": report_dict['macro avg']['precision'], \
+    #         f"{prefix}/{aggregation}/recall": report_dict['macro avg']['recall'], \
+    #         f"{prefix}/{aggregation}/f1-score": report_dict['macro avg']['f1-score'] 
+    #     })  
